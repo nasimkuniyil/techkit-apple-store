b51a087f0e17a8cd156a7d943a39408f81ac2b20
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-02-27 18:35:11 +0530

    pagination added in orders list in admin side

--- controller/admin/page/orderPageController.js
+++ controller/admin/page/orderPageController.js
@@ -5,18 +5,26 @@ const Order = require("../../../models/orderSchema");
 // ORDER PAGE
 const ordersPage = async (req, res, next) => {
   try {
+    const currentPage = req.query.page || 1;
+    const limit = 5;
+    const skip = (currentPage-1) * limit;
     const orders = await Order.find()
       .populate("addressInfo")
       .populate("products.productId")
-      .sort({ createdAt: -1 });
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
 
     if (!orders) {
       const error = new Error("Order not found");
       error.status = 404;
       return next(error);
     }
+
+    const totalOrders = await Order.countDocuments();
+    const totalPage = Math.ceil(totalOrders/limit);
     
-    res.render("admin/orderList", { orders });
+    res.render("admin/orderList", { orders, totalPage, currentPage });
   } catch (err) {
     next(err);
   }
