96da5ee9a8aa0ecb108d6301f27dc000c693c64b
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-02-27 10:51:07 +0530

    moved mongodb uri from connectDB to .env file

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,42 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports.connectDB = async function(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/techkit')
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log('Database connected'))
     .catch((err)=>console.log('mongoose error message : ',err));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //{ //define a schema
-    // const adminSchema = new mongoose.Schema({
-    //     email : String,
-    //     password : String
-    // });
-
-    // // define model
-    // const Admin = mongoose.model('admin', adminSchema)}
